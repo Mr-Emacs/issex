680fc780182e39c82882b25de6a18bb22fcf74f7
Mr-Emacs <tadihailukebe@gmail.com>
2026-04-13 02:57:38 -0400

    [query] Query based language in the working.
    This is just me adding a query language that does the filtering mechanism.

--- nob.c
+++ nob.c
@@ -3,23 +3,43 @@
 
 #if defined(__clang__)
 #define CMD "clang"
-#else
+#elif defined(__GNUC__)
 #define CMD "cc"
+#else
+#error "Unsupported Compiler install GCC or CLANG"
 #endif
+
+#ifdef _WIN32
+    const char *out   = "issex.exe";
+    const char *query = "query.exe";
+#else
+    const char *out   = "issex";
+    const char *query = "query";
+#endif
+
+void common_cmd(Cmd *cmd)
+{
+    cmd_append(cmd, CMD);
+    cmd_append(cmd, "-Wno-deprecated-declarations");
+    cmd_append(cmd, "-ggdb", "-Wall", "-Wextra");
+    cmd_append(cmd, "-std=c23");
+    cmd_append(cmd, "-Wno-unused-function");
+}
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
-    Cmd cmd = {0};
+    static Cmd   cmd   = {0};
+    static Procs procs = {0};
 
-    #ifdef _WIN32
-    const char *out = "issex.exe";
-    #else
-    const char *out = "issex";
-    #endif
-    cmd_append(&cmd, CMD);
-    cmd_append(&cmd, "-Wno-deprecated-declarations", "-ggdb", "-Wall", "-Wextra");
+    common_cmd(&cmd);
     cmd_append(&cmd, "-o", out, "main.c");
-    if (!nob_cmd_run(&cmd)) return 1;
+    if (!nob_cmd_run(&cmd, .async = &procs)) return 1;
+
+    common_cmd(&cmd);
+    cmd_append(&cmd, "-o", out, "query.c");
+    if (!nob_cmd_run(&cmd, .async = &procs)) return 1;
+
+    if (!nob_procs_flush(&procs)) return 1;
     return 0;
 }
